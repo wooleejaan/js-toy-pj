45619afd4a7d75862fc9b8d1b7399ebda0159562
woolee <wooleejaan@gmail.com>
2022-10-25 23:50:43 +0900

    무한 스크롤 2

---
+++ infinite scrolling_2/app.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container')
+const URL = 'https://i.pravatar.cc/500?img='
+
+
+function getRandNum(){
+  return Math.floor(Math.random() * 70) // 0~69
+}
+
+function loadImages(numImages = 10){
+  let i=0;
+  while (i < numImages){
+    const img = document.createElement('img')
+    img.src = `${URL}${getRandNum()}`
+    container.appendChild(img)
+    i++
+  }
+}
+
+loadImages()
+
+window.addEventListener('scroll', () => {
+  // console.log(window.scrollY)
+  // console.log(window.innerHeight)
+
+  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+    loadImages()
+  }
+})
