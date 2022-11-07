86efe7b7f8c6d8d9cbc9800f2b3ce526f2848fc3
woolee <wooleejaan@gmail.com>
2022-11-08 00:02:13 +0900

    intro screen

--- introScreen/app.js
+++ introScreen/app.js
@@ -1,3 +1,15 @@
+const header = document.querySelector('.header');
+
+window.onscroll = function() {
+  let top = window.scrollY;
+
+  if (top >= 100) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+}
+
 const splash = document.querySelector('.splash');
 
 document.addEventListener('DOMContentLoaded', (e) => {
