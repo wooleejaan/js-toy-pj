8279e3c529fa0c8f94858c12c3f30f7ff5f2450b
woolee <wooleejaan@gmail.com>
2022-11-11 17:54:17 +0900

    d

--- clipboard /app.js
+++ clipboard /app.js
@@ -3,11 +3,12 @@ let copyText = document.querySelector('.copy-text');
 copyText.querySelector('button').addEventListener('click', async function () {
   let input = copyText.querySelector('input.text');
 
+  // navigator.clipboard.writeText()로 값을 받아와서 readText()로 뿌려주는 방법 
   await navigator.clipboard.writeText(input.value);
   copyText.classList.add('active');
 
   setTimeout(async function () {
-    // await 처리 안하면 text에 값이 담기지 않는다..
+    // await 처리 안하면 text에 값이 안 담기네..
     const text = await navigator.clipboard.readText();
     alert(`${text}가 복사되었어요.`);
     copyText.classList.remove('active');
