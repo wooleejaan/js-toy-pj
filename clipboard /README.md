51e89fbf572e2b2dab7a16603752807a40ccc316
woolee <wooleejaan@gmail.com>
2022-11-11 17:16:29 +0900

    clipboard

---
+++ clipboard /README.md
@@ -0,0 +1,10 @@
+### tutorial 출처 
+https://www.youtube.com/watch?v=7c1K1SCmCb4&t=2s
+https://www.youtube.com/watch?v=9-vBx7F0lns
+
+### 클립보드 복사하는 방법 3가지 
+1. document.execCommand() : 지금 사용안함
+2. Clipboad API
+3. clipboard.js
+
+https://wildeveloperetrain.tistory.com/151
---
+++ clipboard /app.js
@@ -0,0 +1,15 @@
+let copyText = document.querySelector('.copy-text');
+
+copyText.querySelector('button').addEventListener('click', async function () {
+  let input = copyText.querySelector('input.text');
+
+  await navigator.clipboard.writeText(input.value);
+  copyText.classList.add('active');
+
+  setTimeout(async function () {
+    // await 처리 안하면 text에 값이 담기지 않는다. 
+    const text = await navigator.clipboard.readText();
+    alert(`${text}가 복사되었어요.`);
+    copyText.classList.remove('active');
+  }, 2500);
+})
