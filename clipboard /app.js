27d92a6fcfedc7869307679d011acd996aad802e
woolee <wooleejaan@gmail.com>
2022-11-11 17:18:15 +0900

    d

--- clipboard /app.js
+++ clipboard /app.js
@@ -7,7 +7,7 @@ copyText.querySelector('button').addEventListener('click', async function () {
   copyText.classList.add('active');
 
   setTimeout(async function () {
-    // await 처리 안하면 text에 값이 담기지 않는다. 
+    // await 처리 안하면 text에 값이 담기지 않는다..
     const text = await navigator.clipboard.readText();
     alert(`${text}가 복사되었어요.`);
     copyText.classList.remove('active');
